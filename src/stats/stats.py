819eda405696500048ac840291c2d2935d9a42cd
jetstrsam <sapantoja45@ucatolica.edu.co>
2026-08-17 17:52:36 -0500

    Se resolvio el ejercicio de media aritmetica.

--- src/stats/stats.py
+++ src/stats/stats.py
@@ -12,7 +12,9 @@ class Stats:
         Ejemplo:
             promedio([1, 2, 3, 4, 5]) -> 3.0
         """
-        pass
+        if not numeros:
+            return 0.0
+        return sum(numeros) / len(numeros)
     
     def mediana(self, numeros):
         """
