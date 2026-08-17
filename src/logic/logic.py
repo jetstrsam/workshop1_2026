77729a94782832416f08c2a727a59581b7fe38dc
jetstrsam <sapantoja45@ucatolica.edu.co>
2026-08-17 15:05:32 -0500

    Se realizo el ejercicio de lógica XNOR.

--- src/logic/logic.py
+++ src/logic/logic.py
@@ -78,7 +78,7 @@ class Logica:
         Returns:
             bool: Resultado de a NOR b
         """
-        pass
+        return not (a or b)
     
     def XNOR(self, a, b):
         """
@@ -91,7 +91,7 @@ class Logica:
         Returns:
             bool: Resultado de a XNOR b
         """
-        pass
+        return a == b
     
     def implicacion(self, a, b):
         """
