2c456f307f57d8b62421913eefe19e822b039061
jetstrsam <sapantoja45@ucatolica.edu.co>
2026-08-17 15:40:16 -0500

    Se resolvio el ejercicio de quitar doble espaciado.

--- src/string/strings.py
+++ src/string/strings.py
@@ -132,7 +132,11 @@ class Strings:
         Returns:
             bool: True si la cadena representa un número entero, False en caso contrario
         """
-        pass
+        try:
+            int(texto)
+            return True
+        except ValueError:
+            return False:
     
     def cifrar_cesar(self, texto, desplazamiento):
         """
