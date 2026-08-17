7da962744b660c2767681faf529e1d95939c3b18
jetstrsam <sapantoja45@ucatolica.edu.co>
2026-08-16 22:42:47 -0500

    Se resolvio el quinto ejercicio con la función incorporada <bin>.Se utilizó [2:] para eliminar los primeros dos carácteres '0b'.

--- src/conversion/conversion.py
+++ src/conversion/conversion.py
@@ -87,7 +87,8 @@ class Conversion:
             decimal_a_binario(10) -> "1010"
             decimal_a_binario(255) -> "11111111"
         """
-        pass
+        dec_to_bin= bin(decimal)[2:]
+        return str(dec_to_bin)
     
     def binario_a_decimal(self, binario):
         """
